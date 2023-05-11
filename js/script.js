4c68df3bc4c18217bd5ffe87060f635f54ac940c
vitor <pvitorv@gmail.com>
2023-05-11 20:59:30 -0300

    primeira leva de commits

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Aqui você pode adicionar lógica para tratar os dados do formulário
+  // por exemplo, enviar os dados para o servidor ou realizar validações
+});
